7b294a98daf65d3411ce114891e941149375fb76
Aaron George <aaron@ometria.com>
2021-09-16 15:59:42 +0100

    Add e2e test

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -91,6 +91,19 @@ func TestCheckE2E(t *testing.T) {
 		},
 
 		{
+			description: "check will only return versions that match the specified path regexps",
+			source: resource.Source{
+				Repository:  "itsdalmo/test-repository",
+				AccessToken: os.Getenv("GITHUB_ACCESS_TOKEN"),
+				Paths:       []string{".*.md"},
+			},
+			version: resource.Version{},
+			expected: resource.CheckResponse{
+				resource.Version{PR: targetPullRequestID, Commit: targetCommitID, CommittedDate: targetDateTime},
+			},
+		},
+
+		{
 			description: "check will skip versions which only match the ignore paths",
 			source: resource.Source{
 				Repository:  "itsdalmo/test-repository",
